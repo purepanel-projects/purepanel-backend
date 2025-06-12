5bd7729eebdf8b4983f64ac5fd18320c4319b874
zyan1226 <zyan1226@163.com>
2025-06-12 10:45:34 +0800

    first commit

---
+++ src/main/java/cn/yzdoit/purepanel/PurepanelBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.yzdoit.purepanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurepanelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurepanelBackendApplication.class, args);
+	}
+
+}
