6c16b5f64bbad701cdcdec8d7af12a50b0cf39b6
zyan1226 <zyan1226@163.com>
2025-06-17 18:35:03 +0800

    swagger注解尝试

--- src/main/java/cn/yzdoit/purepanel/controller/PermissionController.java
+++ src/main/java/cn/yzdoit/purepanel/controller/PermissionController.java
@@ -5,6 +5,8 @@ import cn.yzdoit.purepanel.pojo.res.PermissionTreeListRes;
 import cn.yzdoit.purepanel.pojo.res.Res;
 import cn.yzdoit.purepanel.service.PermissionService;
 import cn.yzdoit.purepanel.utils.TreeListUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.List;
  * @author 闫政
  * @since 2025/6/17 15:07 星期二
  */
+@Tag(name = "菜单权限相关接口")
 @RestController
 @RequestMapping("/permission")
 @RequiredArgsConstructor
@@ -24,6 +27,7 @@ public class PermissionController {
     private final PermissionService permissionService;
 
     @GetMapping("/treeList")
+    @Operation(summary = "获取树形列表")
     public Res<List<PermissionTreeListRes>> treeList() {
         List<Permission> list = permissionService.list();
         List<PermissionTreeListRes> tree = TreeListUtils.toTree(list, Permission::getId, Permission::getPid
