68be4fc5bf01110008356ebd56c1ce1d5b86e318
zyan1226 <zyan1226@163.com>
2025-07-01 19:43:34 +0800

    没干啥

--- src/main/java/cn/yzdoit/purepanel/controller/SysUserController.java
+++ src/main/java/cn/yzdoit/purepanel/controller/SysUserController.java
@@ -1,7 +1,11 @@
 package cn.yzdoit.purepanel.controller;
 
+import cn.yzdoit.purepanel.pojo.entity.SysUser;
+import cn.yzdoit.purepanel.pojo.res.Res;
 import cn.yzdoit.purepanel.service.SysUserService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +23,9 @@ public class SysUserController {
     private final SysUserService sysUserService;
 
 
+    @PostMapping("/pageList")
+    public Res<IPage<SysUser>> pageList(){
+
+        return null;
+    }
 }
