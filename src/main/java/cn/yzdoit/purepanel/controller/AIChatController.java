d0c903230c3e16a812a16d986c7894d83bde1360
zyan1226 <zyan1226@163.com>
2025-08-22 23:56:00 +0800

    新增会话列表查询接口

--- src/main/java/cn/yzdoit/purepanel/controller/AIChatController.java
+++ src/main/java/cn/yzdoit/purepanel/controller/AIChatController.java
@@ -1,13 +1,19 @@
 package cn.yzdoit.purepanel.controller;
 
+import cn.yzdoit.purepanel.pojo.entity.AiChatbotConversation;
 import cn.yzdoit.purepanel.pojo.req.AIChatReq;
+import cn.yzdoit.purepanel.pojo.res.Res;
 import cn.yzdoit.purepanel.service.AIChatService;
+import cn.yzdoit.purepanel.service.AiChatbotConversationService;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * 聊天机器人相关接口
@@ -22,10 +28,19 @@ import reactor.core.publisher.Flux;
 public class AIChatController {
 
     private final AIChatService aiChatService;
+    private final AiChatbotConversationService aiChatbotConversationService;
 
     @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @Operation(summary = "AI 聊天")
     public Flux<String> chat(@RequestBody AIChatReq req, @RequestAttribute String loginUserId) {
         return aiChatService.chat(req, loginUserId);
+    }
+
+    @GetMapping("listConversation")
+    @Operation(summary = "查询会话列表")
+    public Res<List<AiChatbotConversation>> listConversation(@RequestAttribute String loginUserId) {
+        return Res.success(aiChatbotConversationService.list(Wrappers.<AiChatbotConversation>lambdaQuery()
+                .eq(AiChatbotConversation::getUserId, loginUserId)
+                .orderByDesc(AiChatbotConversation::getCreateTime)));
     }
 }
